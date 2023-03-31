5cf897ed7759934d50871fa8d950d7edb1fd8429
Flávio Félix <fau_felix30@hotmail.com>
2023-03-31 00:25:36 -0300

    Feito o desafio 3 e 4 da aula 2

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,11 +1,17 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.awt.BasicStroke;
 
 
 
@@ -34,7 +40,7 @@ public class GeradoraDeFigurinhas {
     graphics.drawImage(imagemOriginal, 0, 0, null);
 
     // Configura uma fonte
-    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 80);
     graphics.setColor(Color.YELLOW);
     graphics.setFont(fonte);
 
@@ -44,7 +50,21 @@ public class GeradoraDeFigurinhas {
     Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
     int larguraTexto = (int) retangulo.getWidth();
     int posicaoTextoX =  (largura - larguraTexto) / 2;
-    graphics.drawString(texto, posicaoTextoX, novaAltura - 100);
+    int posicaoTextoY = novaAltura - 100;
+    graphics.drawString(texto, posicaoTextoX, posicaoTextoY);
+
+    FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+    var textLayout =  new TextLayout(texto, fonte, fontRenderContext);
+    Shape outline =  textLayout.getOutline(null);
+    AffineTransform transform =  graphics.getTransform();
+    transform.translate(posicaoTextoX, posicaoTextoY);
+    graphics.setTransform(transform);
+
+    var outlineStroke = new BasicStroke(largura * 0.004f);
+    graphics.setStroke(outlineStroke);
+    graphics.setColor(Color.BLACK);
+    graphics.draw(outline);
+    graphics.setClip(outline);
 
     // Escrever nova imagem em um arquivo
     ImageIO.write(novaImagem, "png", new File(nomeArquivo));
