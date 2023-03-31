928cbba9dcfc574e0db7cdca04566f3452e54b55
Flávio Félix <fau_felix30@hotmail.com>
2023-03-30 22:55:58 -0300

    Feito o desafio 1 da aula 2

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -10,32 +11,33 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         // Fazer uma conexão HTTP e buscar os 250 séries
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse <String> response = client.send(request, BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        
 
         // extrair as informações que são necessárias. (titulo, poster, classficação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        
-        // exibir e manipular os dados
-        var geradora =  new GeradoraDeFigurinhas();
 
-        for (Map<String,String> filme : listaDeFilmes) {
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
+
+        // exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
+
+        for (Map<String, String> filme : listaDeFilmes) {
 
             String urlImage = filme.get("image");
             String titulo = filme.get("title");
 
             InputStream inputStream = new URL(urlImage).openStream();
-            String nomeArquivo = titulo + ".png";
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
 
-            
             geradora.cria(inputStream, nomeArquivo);
 
             System.out.println(titulo);
